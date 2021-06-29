e5ede77e156e81a01c358055b810932fc02f6f58
iSAlexHere <alexjokercatalin@gmail.com>
2021-06-29 16:56:21 +0200

    Update utils.lua

--- resources/[esx]/[esx-basics]/esx_vehicleshop/client/utils.lua
+++ resources/[esx]/[esx-basics]/esx_vehicleshop/client/utils.lua
@@ -19,7 +19,7 @@ function GeneratePlate()
 			generatedPlate = string.upper(GetRandomLetter(Config.PlateLetters) .. GetRandomNumber(Config.PlateNumbers))
 		end
 
-		ESX.TriggerServerCallback('esx_vehicleshop:isPlateTaken', function(isPlateTaken)
+		ESX.TriggerServerCallback('esx_vehicleshop:isPlateTaken', function (isPlateTaken)
 			if not isPlateTaken then
 				doBreak = true
 			end
@@ -49,7 +49,7 @@ function IsPlateTaken(plate)
 end
 
 function GetRandomNumber(length)
-	Citizen.Wait(0)
+	Citizen.Wait(1)
 	math.randomseed(GetGameTimer())
 	if length > 0 then
 		return GetRandomNumber(length - 1) .. NumberCharset[math.random(1, #NumberCharset)]
@@ -59,7 +59,7 @@ function GetRandomNumber(length)
 end
 
 function GetRandomLetter(length)
-	Citizen.Wait(0)
+	Citizen.Wait(1)
 	math.randomseed(GetGameTimer())
 	if length > 0 then
 		return GetRandomLetter(length - 1) .. Charset[math.random(1, #Charset)]

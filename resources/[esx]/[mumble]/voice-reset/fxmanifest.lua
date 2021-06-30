31a8b932b6eaa46d6a045ed81ba1ee0bcdc7cc5f
IKER <xxikerxx787@gmail.com>
2021-07-01 01:27:24 +0200

    Added command to reset voice

---
+++ resources/[esx]/[mumble]/voice-reset/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+description 'Simple Voice Reset with Command'
+
+version '1.0'
+
+client_script{
+
+    'main.lua'
+
+} 
---
+++ resources/[esx]/[mumble]/voice-reset/main.lua
@@ -0,0 +1,23 @@
+ESX = nil
+
+Citizen.CreateThread(function()
+    while ESX == nil do
+      TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+      Citizen.Wait(0)
+      end
+  end)
+
+  RegisterCommand('resetvoice', function() --  chat command, You can change it to your liking.
+  NetworkClearVoiceChannel()
+  NetworkSessionVoiceLeave()
+  Wait(50)
+  NetworkSetVoiceActive(false)
+  MumbleClearVoiceTarget(2)
+  Wait(1000)
+  MumbleSetVoiceTarget(2)
+  NetworkSetVoiceActive(true)
+  ESX.ShowNotification('Voice chat has been restored.')
+  
+end)
+
+-- To change the Notification change line 19. ;)
